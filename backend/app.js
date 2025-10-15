bcf1f7cc44cf9f407458ecf52c0da002139afc0b
anoush24 <anoushkap247@gmail.com>
2025-10-15 22:36:30 +0530

    app.js changes

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,13 @@ const cors = require("cors");
 const cloudinary = require("cloudinary").v2;
 
 
-// const blogRoutes = require("./routes/blog.routes.js");
+const blogRoutes = require("./routes/blog.routes.js");
 const userRoutes = require("./routes/user.routes.js");
-// const hospRoutes = require("./routes/hosp.routes.js");
-// const adminRoutes = require("./routes/admin.routes.js");
+const hospRoutes = require("./routes/hosp.routes.js");
+const adminRoutes = require("./routes/admin.routes.js");
 const orderRoutes = require("./routes/order.routes.js");
 const productRoutes = require("./routes/product.routes.js");
-// const rescRoutes = require("./routes/resc.routes.js")
+const rescRoutes = require("./routes/resc.routes.js");
 
 
 const app = express();
@@ -17,13 +17,13 @@ app.use(cors());
 app.use(express.json());
 
 
-// app.use("/blogs", blogRoutes);
+app.use("/blogs", blogRoutes);
 app.use("/users", userRoutes);
-// app.use("/hospital", hospRoutes);
-// app.use("/admin", adminRoutes);
+app.use("/hospital", hospRoutes);
+app.use("/admin", adminRoutes);
 app.use("/order", orderRoutes);
 app.use("/product", productRoutes);
-// app.use("/resc", rescRoutes);
+app.use("/resc", rescRoutes);
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
